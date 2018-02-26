9f72aac1f0e52d94b109fcf06ec1a698d54ae915
Liubou Shchupak <36640822+veseja@users.noreply.github.com>
2018-02-26 15:39:33 +0300

    sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2 == 1) return false;
+    else 
+    {
+        var check = [];
+        for (var i = 0; i < bracketsConfig.length; i++)
+        {
+            var pair = bracketsConfig[i];
+            var near = pair[0] + pair[1];
+            check[i] = near;
+        }
+        for (var i = 0; i < (str.length * 18) ; i++)
+        {
+            for (var j = 0; j < check.length; j++)
+            {
+                str = str.replace(check[j], "")
+            }
+        }
+        if (str.length > 0) return false;
+        else return true;
+    }
+
 }
